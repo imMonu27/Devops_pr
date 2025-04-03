718e0513f8cd3fb77edc3f29e59d7fb02b73892c
Mohit Khairnar <mohitkhairnar99@gmail.com>
2025-04-04 01:45:22 +0530

    added nginx to ec2 instances

--- security_group.tf
+++ security_group.tf
@@ -5,7 +5,14 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to all (not recommended for production)
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   egress {
