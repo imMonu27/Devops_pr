b12ff5c5130fb6e6f4eeea0d58e73970085bd3b6
Mohit Khairnar <mohitkhairnar99@gmail.com>
2025-04-03 14:57:32 +0530

    added security group and key pair for ssh connnectivity

---
+++ security_group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.first_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all (not recommended for production)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
